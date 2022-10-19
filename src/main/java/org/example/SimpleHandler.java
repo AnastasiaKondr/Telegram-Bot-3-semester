06085e4c59ba7f8da4c2876c9632148c6d27b90f
Anastasia <nk-news@mail.ru>
2022-10-19 09:34:10 +0500

    Delete class Task because it replace in SimpleHandler

--- src/main/java/org/example/SimpleHandler.java
+++ src/main/java/org/example/SimpleHandler.java
@@ -5,5 +5,25 @@ public class SimpleHandler implements Handler {
     @Override
     public BotResponse returnResponse(BotRequest request){
         return new BotResponse(request.getMessage());
-    };
+    }
+
+    @Override
+    public void doneTask() {
+
+    }
+
+    @Override
+    public void changeTask() {
+
+    }
+
+    @Override
+    public void updateTask() {
+
+    }
+
+    @Override
+    public void deleteTask() {
+
+    }
 }
