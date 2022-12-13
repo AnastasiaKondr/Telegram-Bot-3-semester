4ac27ef30574272d3d5787471c74f048c31a0bad
Anastasia <nk-news@mail.ru>
2022-12-14 01:34:57 +0500

    Бот отзывается на start

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -2,47 +2,59 @@ package org.example;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
-    public static void main(String[] args) {
-        ApiContextInitializer.init();
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-        try {
-            TelegramBotsApi.registerBot(new Bot());
-        }
-        catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
+    private final String botName;
+    private final String botToken;
+
+    public Bot(String botName, String botToken) {
+        this.botName = botName;
+        this.botToken = botToken;
     }
 
     @Override
     public String getBotUsername() {
-        return "SimpleToDoBot";
+        return botName;
     }
 
     @Override
     public String getBotToken() {
-        return "5655163111:AAF9EpSGi-x_NwPtNCWEFWmtsuOM_080Dms";
+        return botToken;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        Message message = update.getMessage();
-        if(message != null && message.hasText()) {
-            switch (message.getText()) {
-                case "/help":
-                    sendMsg(message, "Чем могу помочь?");
-                    break;
-                case "/setting":
-                    sendMsg(message, "Что будем настраивать?");
-                    break;
-                default:
+        try{
+            if(update.hasMessage() && update.getMessage().hasText())
+            {
+                Message inMess = update.getMessage();                // Извлекаем из объекта сообщение пользователя
+                String chatId = inMess.getChatId().toString();      // Достаем из inMess id чата пользователя
+                String response = parseMessage(inMess.getText());  // Получаем текст сообщения пользователя,
+                // Отправляем в написанный нами обработчик
+                SendMessage outMess = new SendMessage();         // Будущий ответ пользователю
 
+                // Добавляем в наше сообщение id чата и наш ответ
+                outMess.setChatId(chatId);
+                outMess.setText(response);
+
+                // Отправка в чат
+                execute(outMess);
             }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
+    }
+    public String parseMessage(String textMsg) {
+        String response = new String();
 
+        //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
+        if(textMsg.equals("/start"))
+            response = "Приветствую, я твой To Do лист! Ты можешь вносить в меня свои задачи." +
+                    " Жми /get, чтобы получить команды";
+        return response;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,35 @@
 package org.example;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
 
         Reader reader = new ReaderConsole();
         Writer writer = new WriterConsole();
         Handler handler = new SimpleHandler();
+
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            Bot bot = new Bot(Main.createBot().getBotUsername(), Main.createBot().getBotToken());
+            telegramBotsApi.registerBot(bot);
+        }
+        catch (TelegramApiException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         while (true) {
             BotRequest request = reader.read();
@@ -15,5 +39,14 @@ public class Main {
             BotResponse response = handler.returnResponse(request);
             writer.write(response);
         }
+    }
+    private static Bot createBot() throws URISyntaxException, IOException {
+        //URL link = Main.class.getResource("application.properties");
+        Properties properties = new Properties();
+        //properties.load(new FileInputStream(new File(link.toURI())));
+        properties.load(new FileInputStream("src/main/resources/application.properties"));
+        String botName = (String) properties.get("bot.name");
+        String botToken = (String) properties.get("bot.token");
+        return new Bot(botName, botToken);
     }
 }
